cf8dc1b7247bd86678ed829a29883e27e0204bfa
Isaac Alao <95632975+Isaac-ALAO@users.noreply.github.com>
2025-02-18 21:33:38 +0800

    Update Dashboard.jsx

--- aupcis/src/modules/Patient/Dashboard.jsx
+++ aupcis/src/modules/Patient/Dashboard.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
-const Dashboard = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+export default function DashBoard() {
+    return(
+        <ul className="DashboardList">
+            <div className="FirstRow">
+                <li className="Card-Icons">
+                    <i class="bi bi-0-circle" id="Font"></i>
+                    <Link to="/Schedule"><button className="SchedAPP">Schedule Appointments</button></Link>
+                </li>
+                <li className="Card-Icons">
+                        <i class="bi bi-0-circle" id="Font"></i>
+                        <Link to="/Profile"><button className="SchedAPP">Check Profile</button></Link>
+                </li>
+            </div>
+            <div className="SecondRow">
+                <li className="Card-Icons">
+                    <i class="bi bi-0-circle" id="Font"></i>
+                    <Link to="/Medical Certificate"><button className="SchedAPP">Medical Certificates</button></Link>
+                </li>
+                <li className="Card-Icons">
+                    <i class="bi bi-0-circle" id="Font"></i>
+                    <Link to="/Feedback"><button className="SchedAPP">Give Feedback</button></Link>
+                </li>
+            </div>
+            <div className="ThirdRow">
+                <li className="Card-Icons">
+                    <i class="bi bi-0-circle" id="Font"></i>
+                    <Link to="/Billing"><button className="SchedAPP">Billing Information</button></Link>
+                </li>
+                <li className="Card-Icons">
+                    <i class="bi bi-0-circle" id="Font"></i>
+                    <Link to="/Appointment History"><button className="SchedAPP">Appointment History</button></Link>
+                </li>
+            </div>
+        </ul>
+    )
 }
-
-export default Dashboard
